33077a96b3e0bc755a80c9b9cd27c37650f3f19e
Abhijit Nag <abhijit52@gmail.com>
2017-08-23 20:37:23 -0500

    First Commit of the sample Java code.

---
+++ src/firstapplication/FirstApplication.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstapplication;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Abhijit
+ */
+public class FirstApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Welcome to the first application in Java!");
+        System.exit(0);
+    }
+    
+}
